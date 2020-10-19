32b1929951b400504af0a359bede79cdf3eeb93a
Arturs Kurzemnieks <arturs.kurzemnieks@gmail.com>
2020-10-19 21:12:40 +0300

    Core layout, page separation, styling for auth

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,42 @@ import http from 'http';
 import express from 'express';
 import socketIo from 'socket.io';
 
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+
 const app = express();
 const port = 3001;
+
+type ImaginaryDBSchema = {
+    users: {
+        username: string,
+        pwdHash: string,
+        color: string,
+    }[],
+}
+const imaginaryDB: ImaginaryDBSchema = {
+    users: [],
+}
+
+
+// passport.use('local', new LocalStrategy({
+//     usernameField: 'username',
+//     passwordField: 'password',
+// }, (username, password, done) => {
+//     // DB call goes here
+
+
+// }));
+
+// app.post('/auth', )
 
 const httpServer = http.createServer(app);
 const io = socketIo(httpServer);
  
 io.on('connection', socket => {
     console.log(`New connection: ${socket.id}`);
+
+    
 });
 
 httpServer.listen(port, () => { console.log (`Listening on port ${port}`)});
